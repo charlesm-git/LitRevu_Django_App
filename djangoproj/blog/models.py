b9f47e1aa7b586bee4870200d0e581e690503452
cmareau-git <charles_mareau@hotmail.fr>
2024-10-15 12:13:12 +0200

    Model Creation and Database setup

--- djangoproj/blog/models.py
+++ djangoproj/blog/models.py
@@ -4,15 +4,20 @@ from django.db import models
 
 
 class Ticket(models.Model):
-    # Your Ticket model definition goes here
-    pass
+    title = models.CharField(max_length=128)
+    description = models.TextField(max_length=128, blank=True)
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
 
 
 class Review(models.Model):
     ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
-    rating = models.PositiveSmallIntegerField(max_length=1024,
+    rating = models.PositiveSmallIntegerField(
         # validates that rating must be between 0 and 5
-        validators=[MinValueValidator(0), MaxValueValidator(5)]
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
     )
     headline = models.CharField(max_length=128)
     body = models.CharField(max_length=8192, blank=True)
@@ -23,8 +28,17 @@ class Review(models.Model):
 
 
 class UserFollows(models.Model):
-    # Your UserFollows model definition goes here
-
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following",
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followed_by",
+    )
+    
     class Meta:
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs
