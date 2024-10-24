674e413b9be26d065655c2766f07b5aa74717291
cmareau-git <charles_mareau@hotmail.fr>
2024-10-24 12:09:30 +0200

    Modifications to fit the WCAG requirements

--- djangoproj/authentication/forms.py
+++ djangoproj/authentication/forms.py
@@ -6,17 +6,17 @@ from django.contrib.auth.forms import UserCreationForm
 class LoginForm(forms.Form):
     username = forms.CharField(
         max_length=63,
-        label="",
+        label="Nom d'utilisateur",
         widget=forms.TextInput(
             attrs={"placeholder": "Nom d'utilisateur", "class": "form-control"}
         ),
     )
     password = forms.CharField(
         max_length=63,
+        label="Mot de passe",
         widget=forms.PasswordInput(
             attrs={"placeholder": "Mot de passe", "class": "form-control"}
         ),
-        label="",
     )
 
 
@@ -27,7 +27,7 @@ class SignupForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields["username"].label = ""
+        self.fields["username"].label = "Nom d'utilisateur"
         self.fields["username"].help_text = ""
         self.fields["username"].widget.attrs[
             "placeholder"
@@ -35,15 +35,18 @@ class SignupForm(UserCreationForm):
         self.fields["username"].widget.attrs["class"] = "form-control"
         self.fields["username"].error_messages[
             "unique"
-        ] = "This username is already taken."
+        ] = "Ce nom d'utilisateur est déjà utilisé."
 
-        self.fields["password1"].label = ""
-        # self.fields["password1"].help_text = ""
+        self.fields["password1"].label = "Mot de passe"
+        self.fields["password1"].help_text = (
+            "- Votre mot de passe doit contenir au moins 8 caractères."
+        )
         self.fields["password1"].widget.attrs["placeholder"] = "Mot de passe"
         self.fields["password1"].widget.attrs["class"] = "form-control"
+        self.fields["password1"].widget.attrs["aria-describedby"] = ""
 
-        self.fields["password2"].label = ""
-        # self.fields["password2"].help_text = ""
+        self.fields["password2"].label = "Confirmation de mot de passe"
+        self.fields["password2"].help_text = ""
         self.fields["password2"].widget.attrs[
             "placeholder"
         ] = "Confirmer le mot de passe"
